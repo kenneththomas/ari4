d28bfe720cdd43a8e40103600b20eb9abb7ad642
kenneththomas <kennethwoo11@gmail.com>
2018-01-19 20:45:29 -0500

    chance the rapper

--- modules/mememgr.py
+++ modules/mememgr.py
@@ -1,5 +1,34 @@
+import random
+
+def chance(x):
+    saychance = random.randint(1,x)
+    if saychance == 1:
+        chancerespond = True
+        print('MemeMgr: Memed with a chance of 1/' + str(x))
+    else:
+        chancerespond = False
+
+    return chancerespond
+
 def memes(message):
-    mememessages=[]
-    if message.startswith('!hello'):
-        mememessages.append('hello!')
+
+
+    mememessages=[] #contains all messages that mememgr will return
+
+    if message == 'hi':
+        chancerespond = chance(6)
+        if chancerespond == True:
+            mememessages.append('a')
+
+    if message == '+':
+        chancerespond = chance(6)
+        if chancerespond == True:
+            mememessages.append('+')
+
+    if message == 'a':
+        chancerespond = chance(7)
+        if chancerespond == True:
+            mememessages.append('a')
+
+
     return mememessages
