1de6bf9d99ec621ace0c340d9362bf3d4baf00b2
tayhemmons <tayhemmons@protonmail.com>
2018-01-21 19:32:13 -0500

    refactor of bwm

--- modules/bannedwordsmgr.py
+++ modules/bannedwordsmgr.py
@@ -1,53 +1,67 @@
 import adminmgr
 
-BannedWords=["netorare"]
+BannedWords = ["netorare"]
+
+BannedWordsEnabled = True
 
 
-def bwadmin(message, user):
+def permission(author):
+    if adminmgr.adminCheck(author) is False:
+        return False
 
-    if message == ('!banword list'):
-        return BannedWords
+
+def bwadmin(message, author):
+    global BannedWordsEnabled
+
+    if message.startswith('!banword'):
+        if permission(author) is False:
+            return
 
     if message.startswith('!banword add'):
-        isAdmin = adminmgr.adminCheck(user)
-        if isAdmin == False:
-            return
         addword = message.split(" ")
         BannedWords.append(addword[2])
         print('BannedWordsMgr: Added ' + addword[2] + ' to banned words list.')
         return BannedWords
 
     if message.startswith('!banword remove'):
-        isAdmin = adminmgr.adminCheck(user)
-        if isAdmin == False:
-            return
         addword = message.split(" ")
         BannedWords.remove(addword[2])
         print('BannedWordsMgr: Added ' + addword[2] + ' to banned words list.')
         return BannedWords
 
-def checkword(message):
+    if message.startswith('!banword enable'):
+        BannedWordsEnabled = True
+        print('BannedWordsMgr: Enabled')
 
+    if message.startswith('!banword disable'):
+        BannedWordsEnabled = False
+        print('BannedWordsMgr: Disabled')
+
+
+def checkword(message):
     for BannedWord in BannedWords:
         if BannedWord in message:
-            print('BannedWordsMgr: deleted message with ' + BannedWord)
+            print('BannedWordsMgr: deleted message with banned phrase: ' + BannedWord)
             return True
 
     if message == ('ntr'):
         print('BannedWordsMgr: deleted ntr message')
         return True
 
-nwordvariants = ['nigga','nigger','niqqa']
+
+nwordvariants = ['nigga', 'nigger', 'niqqa']
 blackpeople = []
 passpeople = []
 
-def nwordcheck(message,author):
+
+def nwordcheck(message, author):
     for nword in nwordvariants:
         if nword in message:
             if isuserblack(author):
                 return True
             else:
                 return False
+
 
 def isuserblack(author):
     if author in blackpeople:
@@ -60,7 +74,8 @@ def isuserblack(author):
         print('BannedWordsMgr: ' + author + ' is not black, deleting message')
         return False
 
-def blackcess(message,author):
+
+def blackcess(message, author):
     if author in blackpeople:
         if message.startswith('!pass give'):
             giveblackcess = message.split(" ")
@@ -70,3 +85,16 @@ def blackcess(message,author):
             removeblackcess = message.split(" ")
             print(author + ' has revoked the rights of ' + removeblackcess[2])
             passpeople.remove(removeblackcess[2])
+
+
+def bwlist(message):
+    if message == ('!banword list'):
+        return BannedWords
+
+
+def bwm(message, author):
+    bwadmin(message, author)
+    bwlist(message)
+    if BannedWordsEnabled is True:
+        checkword(message)
+        nwordcheck(message, author)
---
+++ test/test.py
@@ -0,0 +1,15 @@
+import sys
+#import modules
+sys.path.insert(0, '../modules')
+import logmgr
+import mememgr
+import bannedwordsmgr
+
+#TEST 1 - Delete Banned Words
+bannedwordsmgr.bwm('netorare','bobby')
+
+#TEST 2 - Disable/Enable Function
+bannedwordsmgr.bwm('!banword disable','breezyexcursion#9570')
+bannedwordsmgr.bwm('netorare','bobby')
+bannedwordsmgr.bwm('!banword enable','breezyexcursion#9570')
+bannedwordsmgr.bwm('netorare','bobby')
